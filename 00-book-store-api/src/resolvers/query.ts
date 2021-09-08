bb018377e7ea38312a465daf4771b896ca28660e
Anartz Mugika Ledo <mugan86@gmail.com>
2021-09-08 10:44:13 +0200

    Más detalles en los resultados de Querys - BookList - Book

--- 00-book-store-api/src/resolvers/query.ts
+++ 00-book-store-api/src/resolvers/query.ts
@@ -16,16 +16,36 @@ const queryResolvers: IResolvers = {
       return `Hola ${args.name}`;
     },
     peopleNumber: () => 189303,
-    booksList: (): Array<IBook> => {
-      return data.books;
+    booksList: (): {
+      status: boolean,
+      message: string,
+      list: Array<IBook>
+    } => {
+      return {
+        status: true,
+        message: "Lista de libros correctamente cargada",
+        list: data.books
+      };
     },
     peoplesList: (): Array<IPeople> => {
       return data.people;
     },
-    book: (_: void, args: {id: string} ): IBook => {
-      return data.books.filter(
+    book: (_: void, args: {id: string} ): {
+      status: boolean,
+      message: string,
+      item: IBook
+    } => {
+      const bookFind = data.books.filter(
         (value) => value.id === args.id
-      )[0]
+      )[0];
+
+      return {
+        status: bookFind === undefined ? false : true,
+        message: bookFind === undefined ? 
+          `Libro con el id ${args.id} no ha sido encontrado`:
+          `Libro con el id ${args.id} ha sido encontrado`,
+        item: bookFind
+      }
     },
     people: (_: void, args: {id: string} ): IPeople => {
       return data.people.filter(
