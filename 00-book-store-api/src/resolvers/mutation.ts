b0007dc6939000c6b8e648707493a4afbb91a79c
Anartz Mugika Ledo <mugan86@gmail.com>
2021-09-10 18:52:29 +0200

    Añadir libros sin validación

--- 00-book-store-api/src/resolvers/mutation.ts
+++ 00-book-store-api/src/resolvers/mutation.ts
@@ -1,12 +1,23 @@
 import { IResolvers } from '@graphql-tools/utils';
+import data from '../data';
 import { IBook } from '../interfaces/book-interface';
 
 
 const mutationResolvers:IResolvers = {
     Mutation: {
-        addBook: (_: void, args: {book: IBook}): boolean => {
-            console.log(args.book)
-            return true;
+        addBook: (_: void, args: {book: IBook}): {
+            status: boolean,
+            message: string,
+            item: IBook
+        } => {
+            const idValue = +data.books[data.books.length - 1].id + 1;
+            args.book.id = String(idValue);
+            (data.books as IBook[]).push(args.book);
+            return {
+                status: true,
+                message: `Libro con el título ${args.book.title} ha sido añadido correctamente`,
+                item: args.book
+            }
         }
         /**
          * addBook(id: ID!): Boolean
