e53ee580b750c47443b03336ea4f03c832b12e1c
Anartz Mugika Ledo <mugan86@gmail.com>
2021-09-10 19:19:50 +0200

    Validando entrada de libro con el título

--- 00-book-store-api/src/resolvers/mutation.ts
+++ 00-book-store-api/src/resolvers/mutation.ts
@@ -1,30 +1,41 @@
-import { IResolvers } from '@graphql-tools/utils';
-import data from '../data';
-import { IBook } from '../interfaces/book-interface';
+import { IResolvers } from "@graphql-tools/utils";
+import data from "../data";
+import { IBook } from "../interfaces/book-interface";
 
-
-const mutationResolvers:IResolvers = {
-    Mutation: {
-        addBook: (_: void, args: {book: IBook}): {
-            status: boolean,
-            message: string,
-            item: IBook
-        } => {
-            const idValue = +data.books[data.books.length - 1].id + 1;
-            args.book.id = String(idValue);
-            (data.books as IBook[]).push(args.book);
-            return {
-                status: true,
-                message: `Libro con el título ${args.book.title} ha sido añadido correctamente`,
-                item: args.book
-            }
-        }
-        /**
+const mutationResolvers: IResolvers = {
+  Mutation: {
+    addBook: (
+      _: void,
+      args: { book: IBook }
+    ): {
+      status: boolean;
+      message: string;
+      item?: IBook;
+    } => {
+      // Validamos si el título existe en uno de los libros existentes (DUPLICADO)
+      if (
+        data.books.filter((value) => value.title === args.book.title).length > 0
+      ) {
+        return {
+          status: false,
+          message: `El libro que estás introduciendo ya existe. Prueba con otro por favor`,
+        };
+      }
+      const idValue = +data.books[data.books.length - 1].id + 1;
+      args.book.id = String(idValue);
+      (data.books as IBook[]).push(args.book);
+      return {
+        status: true,
+        message: `Libro con el título ${args.book.title} ha sido añadido correctamente`,
+        item: args.book,
+      };
+    },
+    /**
          * addBook(id: ID!): Boolean
   updateBook(id: ID!): Boolean
   deleteBook(id: ID!): Boolean
          */
-    }
-}
+  },
+};
 
 export default mutationResolvers;
