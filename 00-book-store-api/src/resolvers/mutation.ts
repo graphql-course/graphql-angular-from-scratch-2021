6d83a67fe7edfe051a39446fe3f421d7a3f995b6
Anartz Mugika Ledo <mugan86@gmail.com>
2021-09-10 10:14:47 +0200

    Input y Enum añadidos para el libro

--- 00-book-store-api/src/resolvers/mutation.ts
+++ 00-book-store-api/src/resolvers/mutation.ts
@@ -1,8 +1,13 @@
 import { IResolvers } from '@graphql-tools/utils';
+import { IBook } from '../interfaces/book-interface';
 
 
 const mutationResolvers:IResolvers = {
     Mutation: {
+        addBook: (_: void, args: {book: IBook}): boolean => {
+            console.log(args.book)
+            return true;
+        }
         /**
          * addBook(id: ID!): Boolean
   updateBook(id: ID!): Boolean
