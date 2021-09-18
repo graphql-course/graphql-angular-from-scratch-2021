f9806dee72ecae19efb612a4f803eda4afbd0f53
Anartz Mugika Ledo <mugan86@gmail.com>
2021-09-18 11:29:47 +0200

    Ejemplo aplicando mezcla de schema en API Proyecto real

--- 00-book-store-api/src/schema/index.ts
+++ 00-book-store-api/src/schema/index.ts
@@ -1,7 +1,17 @@
 import "graphql-import-node";
 import { makeExecutableSchema } from "@graphql-tools/schema";
 import { GraphQLSchema } from "graphql";
-import typeDefs from "./schema.graphql";
+
+import path from "path";
+import { loadFilesSync } from "@graphql-tools/load-files";
+import { mergeTypeDefs } from "@graphql-tools/merge";
+
+const typesArray = loadFilesSync(path.join(__dirname, "./graphql"), {
+  extensions: ["graphql"],
+});
+
+const typeDefs = mergeTypeDefs(typesArray);
+
 import resolvers from "./../resolvers";
 const schema: GraphQLSchema = makeExecutableSchema({
   typeDefs,
