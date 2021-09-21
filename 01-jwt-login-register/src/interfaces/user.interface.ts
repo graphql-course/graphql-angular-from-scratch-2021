7c43df35c4d3704addd3f2b6fde4157ee10a7c37
Anartz Mugika Ledo <mugan86@gmail.com>
2021-09-21 19:36:36 +0200

    Query - Users - Sin BBDD - Data Fake

---
+++ 01-jwt-login-register/src/interfaces/user.interface.ts
@@ -0,0 +1,8 @@
+export interface IUser {
+    id?: string;
+    name: string;
+    lastname: string;
+    email: string;
+    password?: string;
+    registerDate: string;
+}
--- 01-jwt-login-register/src/resolvers/query.ts
+++ 01-jwt-login-register/src/resolvers/query.ts
@@ -1,8 +1,19 @@
 import { IResolvers } from "@graphql-tools/utils";
+import { IUser } from "../interfaces/user.interface";
 
 const queryResolvers: IResolvers = {
   Query: {
-    
+    users: () : Array<IUser> => {
+      return [
+        {
+          id: "1",
+          name: "Anartz",
+          lastname: "Mugika Ledo",
+          email: "",
+          registerDate: ""
+        }
+      ];
+    }
   },
 };
 
