ababc9714338342b508b465a9ef2362b75d84c21
Anartz Mugika Ledo <mugan86@gmail.com>
2021-09-22 20:42:00 +0200

    Query -  login

--- 01-jwt-login-register/src/resolvers/query.ts
+++ 01-jwt-login-register/src/resolvers/query.ts
@@ -6,6 +6,35 @@ const queryResolvers: IResolvers = {
   Query: {
     users: async(_: void, __: unknown, context: { db: Db}) : Promise<Array<IUser>> => {
       return await context.db.collection("users").find().toArray() as Array<IUser>;
+    },
+    login: async(_: void, args: {
+      email: string, password: string
+    }, context: { db: Db}): Promise<{
+      status: boolean,
+      message: string,
+      user?: IUser
+    }> => {
+      return await context.db.collection("users").findOne(
+        { email: args.email, password: args.password}
+      ).then((user) => {
+        if (!user) {
+          return {
+            status: false,
+            message: "Usuario NO encontrado"
+          };
+        }
+        delete user?._id;
+        return {
+          status: true,
+          message: "Usuario correctamente cargado",
+          user: user as IUser
+        };
+      }).catch((error) => {
+        return {
+          status: false,
+          message: `Error: ${error}`
+        };
+      });
     }
   },
 };
