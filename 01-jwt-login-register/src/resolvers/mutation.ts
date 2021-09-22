993746783edd2f807337f8b9c62ac86db2dd39ef
Anartz Mugika Ledo <mugan86@gmail.com>
2021-09-22 19:25:04 +0200

    Verificaciones OK

--- 01-jwt-login-register/src/resolvers/mutation.ts
+++ 01-jwt-login-register/src/resolvers/mutation.ts
@@ -4,38 +4,60 @@ import { Db } from "mongodb";
 
 const mutationResolvers: IResolvers = {
   Mutation: {
-    add: async(_: void, args: {user: IUser}, context: {db: Db}): Promise<boolean> => {
+    add: async (
+      _: void,
+      args: { user: IUser },
+      context: { db: Db }
+    ): Promise<boolean> => {
       // Comprobar si existe el usuario en la base de datos con el correo
       // Si existe, error mostrando feedback
+      const userCheck = await context.db.collection("users").findOne(
+        { email: args.user.email}
+      );
+
+      if (userCheck) {
+        console.log("Usuario existe y no podemos registrarnos");
+        return false;
+      }
 
       // Usuario sin password definido, mostrar error si se cumple
+      if (!args.user.password) {
+        console.log("Password no establecido / asignado");
+        return false;
+      }
 
       // YA TENEMOS LO NECESARIO PARA AÑADIR
       // Si todo va bien y tenemos la información para almacenar
       // Asignar el ID nuevo automáticamente, basándonos
-      const lastElement = await context.db.collection("users")
-      .find().limit(1).sort({registerDate: -1}).toArray();
+      const lastElement = await context.db
+        .collection("users")
+        .find()
+        .limit(1)
+        .sort({ registerDate: -1 })
+        .toArray();
 
-      args.user.id = (lastElement.length === 0) ? "1": String(+lastElement[0].id + 1);
+      args.user.id =
+        lastElement.length === 0 ? "1" : String(+lastElement[0].id + 1);
 
       console.log(args.user);
-      
 
       // Añadir la fecha de registro antes de almacenar
       args.user.registerDate = new Date().toISOString();
       // Encriptar password antes de almacenar
 
       // Insertar el usuario en la base de datos
-      return await context.db.collection("users").insertOne(args.user)
-      .then(() => {
-        console.log("Añadido correctamente");
-        return true;
-      })
-      .catch((error) => {
-        console.log(`ERROR: ${error}`);
-        return false;
-      });
-    }
+      return await context.db
+        .collection("users")
+        .insertOne(args.user)
+        .then(() => {
+          console.log("Añadido correctamente");
+          return true;
+        })
+        .catch((error) => {
+          console.log(`ERROR: ${error}`);
+          return false;
+        });
+    },
   },
 };
 
