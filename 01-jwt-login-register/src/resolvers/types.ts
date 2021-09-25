4a7a97400d618b60bb50845c491699e2094a7dcc
Anartz Mugika Ledo <mugan86@gmail.com>
2021-09-25 19:52:33 +0200

    Implementar Result Interface en ResultToken y ResultUser

---
+++ 01-jwt-login-register/src/resolvers/types.ts
@@ -0,0 +1,21 @@
+import { IResolvers } from "@graphql-tools/utils";
+import { IUser } from "../interfaces/user.interface";
+
+
+const typesResolvers: IResolvers = {
+    Result: {
+        __resolveType(root: { user: IUser, token: string}){
+          // Respuesta con ResultUser
+          if(root.user){
+            return "ResultUser";
+          }
+          // Respuesta con ResultToken
+          if(root.token){
+            return "ResultToken";
+          }
+          return null; // GraphQLError is thrown
+        },
+      },
+};
+
+export default typesResolvers;
